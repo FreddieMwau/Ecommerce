699d92cf047e63cee06c43648d18918f634f4d84
FreddieMwau <fredrickmutua12@gmail.com>
2022-06-17 13:26:46 +0300

    Implemented reactive forms on the existing forms and designs more pages on the admin page

--- Client/src/app/Admin/admin.module.ts
+++ Client/src/app/Admin/admin.module.ts
@@ -3,19 +3,30 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AngularLineawesomeModule } from 'angular-line-awesome';
+import { AddProductComponent } from './add-product/add-product.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DashDataComponent } from './dash-data/dash-data.component';
+import { EditOrderComponent } from './edit-order/edit-order.component';
 
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    AddProductComponent,
+    WelcomeComponent,
+    DashDataComponent,
+    EditOrderComponent
   ],
   imports: [
     CommonModule,
     AngularLineawesomeModule,
     RouterModule.forChild([
       {path:'', component: DashboardComponent, children:[
-
+        {path:'', component: WelcomeComponent},
+        {path:'home', component: DashDataComponent},
+        { path: 'addproduct', component: AddProductComponent },
+        { path: 'editorder', component: EditOrderComponent }
       ]}
     ])
   ]
---
+++ Client/src/app/Admin/dash-data/dash-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dash-data',
+  templateUrl: './dash-data.component.html',
+  styleUrls: ['./dash-data.component.css']
+})
+export class DashDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
