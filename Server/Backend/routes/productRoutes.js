e7d29e3914c8c5033e2370665147e74886273bb7
FreddieMwau <fredrickmutua12@gmail.com>
2022-06-17 14:17:18 +0300

    Folder structure for db and procedures

--- Server/Backend/routes/productRoutes.js
+++ Server/Backend/routes/productRoutes.js
@@ -5,11 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const productController_1 = require("../controllers/productController");
+const jwtVerify_1 = require("../middleware/jwtVerify");
 const productRouter = express_1.default.Router();
-productRouter.post('/newproduct', productController_1.newProduct);
-productRouter.get('/', productController_1.getAllProducts);
-productRouter.get('/category/:product_category', productController_1.getCategoryProducts);
-productRouter.get('/:product_id', productController_1.getProduct);
-productRouter.delete('/:product_id', productController_1.deleteProduct);
-productRouter.patch('/:product_id', productController_1.updateProduct);
+productRouter.post('/newproduct', jwtVerify_1.verifyToken, productController_1.newProduct);
+productRouter.get('/', jwtVerify_1.verifyToken, productController_1.getAllProducts);
+productRouter.get('/category/:product_category', jwtVerify_1.verifyToken, productController_1.getCategoryProducts);
+productRouter.get('/:product_id', jwtVerify_1.verifyToken, productController_1.getProduct);
+productRouter.delete('/:product_id', jwtVerify_1.verifyToken, productController_1.deleteProduct);
+productRouter.patch('/:product_id', jwtVerify_1.verifyToken, productController_1.updateProduct);
 exports.default = productRouter;
--- Server/Backend/routes/productRoutes.ts
+++ Server/Backend/routes/productRoutes.ts
@@ -1,13 +1,13 @@
 import express from 'express'
 import { deleteProduct, getAllProducts, getCategoryProducts, getProduct, newProduct, updateProduct } from '../controllers/productController'
-
+import { verifyToken } from '../middleware/jwtVerify'
 const productRouter = express.Router()
 
-productRouter.post('/newproduct', newProduct)
-productRouter.get('/', getAllProducts)
-productRouter.get('/category/:product_category', getCategoryProducts)
-productRouter.get('/:product_id', getProduct)
-productRouter.delete('/:product_id', deleteProduct)
-productRouter.patch('/:product_id', updateProduct)
+productRouter.post('/newproduct', verifyToken, newProduct)
+productRouter.get('/', verifyToken,  getAllProducts)
+productRouter.get('/category/:product_category', verifyToken, getCategoryProducts)
+productRouter.get('/:product_id', verifyToken, getProduct)
+productRouter.delete('/:product_id', verifyToken, deleteProduct)
+productRouter.patch('/:product_id', verifyToken, updateProduct)
 
 export default productRouter
